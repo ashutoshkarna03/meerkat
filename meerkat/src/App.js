3a3fbf59c5f8018adc1da947ddc89964c55bea4a
ashutoshkarna03 <ashutosh@uxcam.com>
2020-06-01 22:52:38 +0545

    added navigation in the homepage

--- meerkat/src/App.js
+++ meerkat/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Example from './components/moviePage'
+import Navbar from './components/homePage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
+        <Example />
         <div className="App-header">
           <h2>Welcome to Meerkat</h2>
         </div>
-        <Example />
       </div>
     );
   }
